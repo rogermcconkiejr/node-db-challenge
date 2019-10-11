f199fcd656480552e37f4419732e0a1fc774bd61
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-11 10:10:34 -0600

    task seed completed

--- seeds/002-tasks.js
+++ seeds/002-tasks.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tasks').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('tasks').insert([
+        {description: 'gather supplies', notes:'go to home depot', completed:false, project_id:1},
+        {description: 'nail wood together', notes:'', completed:false, project_id:1},
+        {description: 'get paint and pencil', notes:'', completed:false, project_id:2},
+        {description: 'paint on the paper', notes:'use small strokes', completed:false, project_id:2},
+        {description: 'go on run', notes:'use shoes', completed:false, project_id:3},
+
       ]);
     });
 };
